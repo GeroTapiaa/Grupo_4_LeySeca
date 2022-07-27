55d1cef3d481c2c2eb5f9573cfae8cde23ef4db0
Eva Coste <CosteEva@outlook.com>
2022-07-27 17:00:11 -0300

    agrego rutas

--- app.js
+++ app.js
@@ -6,5 +6,7 @@ app.use(express.static("public"))
 
 
 
-
+app.get('/', (req,res) => res.sendFile(path.join(__dirname, 'views', 'register.html' )));
+app.get('/login', (req,res) => res.sendFile(path.join(__dirname, 'views', 'login.html' )));
+app.get('/Ter-Condiciones', (req,res) => res.sendFile(path.join(__dirname, 'views', 'Ter-condiciones.html' )));
 app.listen(3030 , () => console.log("Servidor corriendo en" + port));
