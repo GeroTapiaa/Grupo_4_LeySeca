a5aa4ad68812baf6de945658c3612c1a5e9c79a3
Nadia Florencia Maldonado <nadiamaldonado3011@gmail.com>
2022-08-01 19:17:18 -0300

    se crearon rutas de login, register y ter-condiciones

--- app.js
+++ app.js
@@ -11,3 +11,5 @@ app.listen(port, () => console.log('Server Up in http://localhost:' + port));
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views', 'index.html')));
 app.get('/register', (req, res) => res.sendFile(path.join(__dirname, 'views', 'register.html')));
+app.get('/login', (req, res) => res.sendFile(path.join(__dirname, 'views', 'login.html')));
+app.get('/terminos', (req, res) => res.sendFile(path.join(__dirname, 'views', 'ter-condiciones.html')));
