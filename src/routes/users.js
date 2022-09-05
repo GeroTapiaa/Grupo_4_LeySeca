cd427aa4d36107b6688fcf2278a2bf8497964346
Nadia Florencia Maldonado <nadiamaldonado3011@gmail.com>
2022-09-05 15:33:31 -0300

    Se creo vista de perfil de usuario y formulario de edición

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var router = express.Router();
-const { login, register } = require('../controllers/userController')
+const { login, register, update, profile, edit } = require('../controllers/userController')
     /* GET users listing. */
 router
     .get('/login', login)
     .get('/register', register)
+    .get('/profile', profile)
+    .get('/profileEdit/:id', edit)
+    .put('/update/:id', update)
 
 
 module.exports = router;
