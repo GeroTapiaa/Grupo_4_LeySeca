46d33ba0183ba30e542b4a7bfc4e9faff461f39f
Nadia Florencia Maldonado <nadiamaldonado3011@gmail.com>
2022-09-07 17:15:51 -0300

    Se valido formulario de registro

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,9 +1,54 @@
+const { loadUsers, storeUsers } = require('../data/db-module');
+const { validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+
+
+const fs = require('fs');
+const path = require('path');
+
 module.exports = {
     login: (req, res) => {
         res.render('user/login')
     },
     register: (req, res) => {
         res.render('user/register')
+    },
+
+    userRegister: (req, res) => {
+
+        const errors = validationResult(req);
+
+
+        if (errors.isEmpty()) {
+            const { name, lastName, user, date, address, email, password, avatar } = req.body;
+            const users = loadUsers();
+
+            newUser = {
+
+                id: users[users.length - 1] ? users[users.length - 1].id + 1 : 1,
+                name: name.trim(),
+                lastName: lastName.trim(),
+                user: user.trim(),
+                date: date,
+                address: address.trim(),
+                email: email.trim(),
+                password: bcrypt.hashSync(password.trim(), 10),
+                avatar,
+                rol: "user"
+
+            }
+            const userModify = [...users, newUser]
+            storeUsers(userModify);
+
+            res.redirect('/users/login');
+        }
+        res.render('user/register', {
+            errors: errors.mapped(),
+            old: req.body
+        })
+
+
+
     },
     profile: (req, res) => {
         res.render('user/profile')
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var router = express.Router();
-const { login, register, update, profile, edit } = require('../controllers/userController')
-    /* GET users listing. */
+const { login, register, update, profile, edit, userRegister } = require('../controllers/userController');
+const registerValidator = require('../validations/registerValidator');
+
 router
     .get('/login', login)
     .get('/register', register)
+    .post('/register', registerValidator, userRegister)
     .get('/profile', profile)
     .get('/profileEdit/:id', edit)
     .put('/update/:id', update)
