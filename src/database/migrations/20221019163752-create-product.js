7650fd1da471b8688fab1509c0f2d29eb16ab4da
GeroTapiaa <gerook98@hotmail.com>
2022-10-20 16:16:17 -0300

    migrations and seed

---
+++ src/database/migrations/20221019163752-create-product.js
@@ -0,0 +1,50 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Products', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull : false
+      },
+      price: {
+        type: Sequelize.INTEGER,
+        allowNull : false
+      },
+      description: {
+        type: Sequelize.STRING,
+        defaultValue : 0
+      },
+      discount: {
+        type: Sequelize.INTEGER
+      },
+      categoryId: {
+        type: Sequelize.INTEGER,
+        references : {
+          model :{
+            tableName : 'Categories'
+          },
+          key : 'id'
+        }
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        type: Sequelize.DATE
+      },
+      deletedAt: {
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Products');
+  }
+};
